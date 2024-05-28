413a021aa9450e3c459c5897c3b85ddd113bb129
Don Function <69618397+MRFUNCT1ON@users.noreply.github.com>
2024-05-28 19:08:56 +1000

    xyz

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@functionisland-dex/sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0xB6cE71Fe5dA13f98f3b2688d50115E9222668E3F'
+  [ChainId.MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441'
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
