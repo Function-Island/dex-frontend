db8f3a78f53027ee94ee4e8ec4e15a2435efe51e
Don Function <69618397+MRFUNCT1ON@users.noreply.github.com>
2024-07-16 20:44:59 +1000

    Update useColor.ts

--- src/hooks/useColor.ts
+++ src/hooks/useColor.ts
@@ -44,7 +44,7 @@ async function getColorFromUriPath(uri: string): Promise<string | null> {
 }
 
 export function useColor(token?: Token) {
-  const [color, setColor] = useState('#444444')
+  const [color, setColor] = useState('#000000')
 
   useLayoutEffect(() => {
     let stale = false
@@ -59,7 +59,7 @@ export function useColor(token?: Token) {
 
     return () => {
       stale = true
-      setColor('#444444')
+      setColor('#000000')
     }
   }, [token])
 
@@ -67,7 +67,7 @@ export function useColor(token?: Token) {
 }
 
 export function useListColor(listImageUri?: string) {
-  const [color, setColor] = useState('#444444')
+  const [color, setColor] = useState('#000000')
 
   useLayoutEffect(() => {
     let stale = false
@@ -82,7 +82,7 @@ export function useListColor(listImageUri?: string) {
 
     return () => {
       stale = true
-      setColor('#444444')
+      setColor('#000000')
     }
   }, [listImageUri])
 
